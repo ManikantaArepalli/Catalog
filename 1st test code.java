3c8faec9ae24880b0e0590d90f5533f339ec232c
Manikanta <97425299+ManikantaArepalli@users.noreply.github.com>
2025-01-29 12:13:27 +0530

    Create 1st test code.java
    code for 1st sample test case

---
+++ 1st test code.java
@@ -0,0 +1,58 @@
+import org.json.JSONObject;
+public class PolynomialConstantTerm {
+    public static int convertToDecimal(String value, int base) {
+        return Integer.parseInt(value, base);
+    }
+    public static int calculateConstantTerm(int[] roots, int m) {
+        int constantTerm = 1;
+        for (int root : roots) {
+            constantTerm *= root;
+        }
+        constantTerm *= Math.pow(-1, m); 
+        return constantTerm;
+    }
+    public static int processPolynomial(JSONObject jsonData) {
+        int n = jsonData.getJSONObject("keys").getInt("n");
+        int k = jsonData.getJSONObject("keys").getInt("k");
+        int m = n - 1;
+        int[] roots = new int[k];
+        int index = 0;
+        for (String key : jsonData.keySet()) {
+            if (!key.equals("keys")) {
+                JSONObject rootInfo = jsonData.getJSONObject(key);
+                int base = rootInfo.getInt("base");
+                String rootValue = rootInfo.getString("value");
+                int decimalRoot = convertToDecimal(rootValue, base);
+                roots[index++] = decimalRoot;
+            }
+        }
+        return calculateConstantTerm(roots, m);
+    }
+    public static void main(String[] args) {
+        String jsonInput = "{\n" +
+                "    \"keys\": {\n" +
+                "        \"n\": 4,\n" +
+                "        \"k\": 3\n" +
+                "    },\n" +
+                "    \"1\": {\n" +
+                "        \"base\": \"10\",\n" +
+                "        \"value\": \"4\"\n" +
+                "    },\n" +
+                "    \"2\": {\n" +
+                "        \"base\": \"2\",\n" +
+                "        \"value\": \"111\"\n" +
+                "    },\n" +
+                "    \"3\": {\n" +
+                "        \"base\": \"10\",\n" +
+                "        \"value\": \"12\"\n" +
+                "    },\n" +
+                "    \"6\": {\n" +
+                "        \"base\": \"4\",\n" +
+                "        \"value\": \"213\"\n" +
+                "    }\n" +
+                "}";
+        JSONObject jsonData = new JSONObject(jsonInput);
+        int constantTerm = processPolynomial(jsonData);
+        System.out.println("The constant term of the polynomial is: " + constantTerm);
+    }
+}
